a83b1c98f270a6c6f8de01703df4d65a68bcb7b1
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-02-26 20:48:39 +0100

    Add v0.246 -> v0.2.51

--- generate-html.ps1
+++ generate-html.ps1
@@ -3,7 +3,7 @@ Set-Location $PSScriptRoot
 $destinationDir = if (Test-Path $(Join-Path $(Resolve-Path '.') 'index')) {Join-Path '.' 'index' -resolve} else {(New-Item 'index' -ItemType 'Directory').fullname}
 $avxVersions = "AVX","AVX2","AVX512","basic"
 $cudaVersions = "12.0","12.1","12.2","rocm5.5.1","rocm5.6.1","rocm5.7.1","cpu"
-$packageVersions = (39..46).foreach({"$_".Insert(0,'0.2.')})
+$packageVersions = (39..51).foreach({"$_".Insert(0,'0.2.')})
 $pythonVersions = "3.10","3.11"
 $supportedSystems = 'linux_x86_64','win_amd64','macosx_11_0_x86_64','macosx_12_0_x86_64','macosx_13_0_x86_64','macosx_14_0_x86_64','macosx_11_0_arm64','macosx_12_0_arm64','macosx_13_0_arm64','macosx_14_0_arm64','macosx_11_0_aarch64','macosx_12_0_aarch64','macosx_13_0_aarch64','macosx_14_0_aarch64'
 $wheelSource = 'https://github.com/smartappli/serge-wheels/releases/download'
